5c3d0066525463db374a58bef7ca69ea729eb4d0
bnf0533-cyber <bnf0533@gmail.com>
2026-07-29 17:50:14 +0300

    add dubget.repo file

--- repository/budget.repo.js
+++ repository/budget.repo.js
@@ -9,8 +9,25 @@ export async function createBudget(body) {
             allocatedAmount: 10,
         };
         // אם יש לי כבר כזה דבר צריך 409 לאותו מידע שקיבלתי בבודי
-        return {status :201 , data : "newBudget"}
+        return { status: 201, data: "newBudget" };
     } catch (error) {
-        throw error
+        throw error;
     }
 }
+
+export async function getBudgetByQuery(data) {
+    try {
+        const get = await client.from("budget").select("*").eq("something", data);
+        return get;
+        // id , benefitType , month , allocatedAmount , spentAmount , remainingAmount(allocatedAmount  - spentAmount)
+    } catch (error) {}
+}
+
+export async function getAllBudgetById(id) {
+    try {
+        const get = client.from("budget").select("*").eq("id", id);
+        return { data: get, status: 200 }; // אם לא נמצא להחזיר בקומטרולר 404
+    } catch (error) {}
+}
+
+
