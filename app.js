4fa3ddb634809769e1d4446f6433c57ba3b2365e
bnf0533-cyber <bnf0533@gmail.com>
2026-07-29 17:51:05 +0300

    add app  file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import dns from "dns"
 import "./db/mongo.db.js"
 import "./db/supabase.db.js"
 import soldierRouter from "./router/solider.route.js"
+import budgetRouter from "./router/budget.route.js"
 
 dns.setServers(["1.1.1.1","8.8.8.8"])
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 
 app.use("/soldiers",soldierRouter)
 
+app.use("/budget", budgetRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("server running...");
