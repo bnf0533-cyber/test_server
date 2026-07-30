e9533a8faed934e77d6210c610187c766679fc42
bnf0533-cyber <bnf0533@gmail.com>
2026-07-30 16:24:32 +0300

    add router folder

--- router/budget.route.js
+++ router/budget.route.js
@@ -1,12 +1,14 @@
-import {createBudgetCtrl , getAllBudgetByIdCtrl , getBudgetByQueryCtrl} from "../controller/budget.ctrl.js"
+import { createBudgetCtrl, getBudgetByQueryCtrl, getAllBudgetByIdCtrl, createSpendCtrl } from "../controller/budget.ctrl.js"
 import express from "express"
 
 const router = express.Router()
 
 router.post("/", createBudgetCtrl)
 
-router.get("/",getBudgetByQueryCtrl)
+router.get("/", getBudgetByQueryCtrl)
 
 router.get("/:id/transactions", getAllBudgetByIdCtrl)
+
+router.post("/:id/spend", createSpendCtrl)
 
 export default router;
--- router/solider.route.js
+++ router/solider.route.js
@@ -1,18 +1,12 @@
-import { createBenefit, getBenefitById } from "../controller/soldier.ctrl.js";
+import { createBenefit, getBenefitById, patchBenefitCtrl } from "../controller/soldier.ctrl.js";
 import express from "express"
 
 const router = express.Router()
 
 router.post("/:soldierId/benefits", createBenefit)
 
-router.get("/:soldierId/benefits",getBenefitById)
+router.get("/:soldierId/benefits", getBenefitById)
 
-router.patch("/:soliderId/benefits", (req , res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-})
+router.patch("/:soldierId/benefits", patchBenefitCtrl)
 
 export default router;
